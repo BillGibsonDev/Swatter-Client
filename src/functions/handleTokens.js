5206aae477af68a2c5076189dd23d215e670420f
DevGibby <71801512+DevGibby@users.noreply.github.com>
2023-01-11 22:18:57 -0500

    Update handleTokens.js

--- src/functions/handleTokens.js
+++ src/functions/handleTokens.js
@@ -1,6 +1,4 @@
 export const handleTokens = (token, username) => {
     sessionStorage.setItem("token", token);
-    localStorage.setItem("token", token);
     sessionStorage.setItem("username", username);
-    localStorage.setItem("username", username);
 };
